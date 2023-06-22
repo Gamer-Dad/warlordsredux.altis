35ac6e97883203c49a60fff7b8a36ebd7d16813d
MrThomasM <thomas.j336699@gmail.com>
2023-06-22 12:36:44 +0200

    AC commands removed

--- mp_security.hpp
+++ mp_security.hpp
@@ -46,14 +46,6 @@ class CfgDisabledCommands {
 			args[] = {{"OBJECT"},{"SCALAR"}};
 		};
 	};
-	class SETPOSASL
-	{
-		class SYNTAX1
-		{
-			targets[] = {1,0,0};
-			args[] = {{"OBJECT"},{"ARRAY"}};
-		};
-	};
 	class SETPOSASLW
 	{
 		class SYNTAX1
@@ -84,20 +76,6 @@ class CfgDisabledCommands {
 		{
 			targets[] = {1,0,0};
 			args[] = {{"OBJECT"},{"ARRAY"}};
-		};
-	};
-	class CREATEUNIT
-	{
-		class SYNTAX1
-		{
-			targets[] = {1,0,0};
-			args[] = {{"STRING"},{"ARRAY"}};
-		};
-
-		class SYNTAX2
-		{
-			targets[] = {1,0,0};
-			args[] = {{"GROUP"},{"ARRAY"}};
 		};
 	};
 	class SETVELOCITY
