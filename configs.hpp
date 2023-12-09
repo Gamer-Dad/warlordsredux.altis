c5acca7d3cf39d385f84cfd6b322dd699fbd47f2
notDevChaw <151213509+notDevChaw@users.noreply.github.com>
2023-12-10 05:23:09 +0600

    faction locked arsenal (rifles)
    That would be the end of the Weapons part of Arsenal Locked for both the factions
    tested on my own local server
    pushing to check on the test server

--- configs.hpp
+++ configs.hpp
@@ -4,12 +4,41 @@ class arsenalConfig
 {
 	class west
 	{
-		Rifles[] = {};
+		Rifles[] = {
+		//*BOTH*//
+			//SMG
+			"SMG_03_TR_black","SMG_03_TR_camo","SMG_03_TR_khaki","SMG_03_TR_hex","SMG_03C_TR_black","SMG_03C_TR_camo","SMG_03C_TR_khaki","SMG_03C_TR_hex","SMG_03_black","SMG_03_camo","SMG_03_khaki","SMG_03_hex","SMG_03C_black","SMG_03C_camo","SMG_03C_khaki","SMG_03C_hex","hgun_PDW2000_F",
+			//Rifles
+			"arifle_AK12_F","arifle_AK12_GL_F","arifle_AK12_lush_F","arifle_AK12_arid_F","arifle_AK12_GL_lush_F","arifle_AK12_GL_arid_F","arifle_MSBS65_F","arifle_MSBS65_GL_F","arifle_MSBS65_UBS_F","arifle_MSBS65_black_F","arifle_MSBS65_GL_black_F","arifle_MSBS65_UBS_black_F","arifle_MSBS65_sand_F","arifle_MSBS65_GL_sand_F","arifle_MSBS65_UBS_sand_F","arifle_MSBS65_camo_F","arifle_MSBS65_GL_camo_F","arifle_MSBS65_UBS_camo_F","arifle_SDAR_F",
+			//Carbines
+			"arifle_AKS_F",
+			//DMR
+			"srifle_DMR_06_hunter_F","arifle_MSBS65_Mark_F","arifle_MSBS65_Mark_black_F","arifle_MSBS65_Mark_sand_F","arifle_MSBS65_Mark_camo_F",
+			//SAW
+			"LMG_03_F",
+			//Shotgun
+			"sgun_HunterShotgun_01_F","sgun_HunterShotgun_01_sawedoff_F",
+		//*BLUEFOR*//
+			//SMG
+			"SMG_01_F",
+			//Rifles
+			"arifle_Mk20_F","arifle_Mk20_plain_F","arifle_Mk20_GL_F","arifle_Mk20_GL_plain_F","arifle_Mk20_MRCO_plain_F","arifle_Mk20_MRCO_pointer_F","arifle_Mk20_GL_MRCO_pointer_F","arifle_MX_F","arifle_MX_GL_F","arifle_MX_Black_F","arifle_MX_GL_Black_F","arifle_MX_khk_F","arifle_MX_GL_khk_F","arifle_SPAR_01_blk_F","arifle_SPAR_01_khk_F","arifle_SPAR_01_snd_F","arifle_SPAR_01_GL_blk_F","arifle_SPAR_01_GL_khk_F","arifle_SPAR_01_GL_snd_F","arifle_TRG21_F","arifle_TRG21_GL_F",
+			//Carbines
+			"arifle_Mk20C_F","arifle_Mk20C_plain_F","arifle_MXC_F","arifle_MXC_khk_F","arifle_MXC_Black_Holo_Pointer_F","arifle_TRG20_F",
+			//DMR
+			"srifle_DMR_02_F","srifle_DMR_02_camo_F","srifle_DMR_02_sniper_F","srifle_DMR_03_F","srifle_DMR_03_khaki_F","srifle_DMR_03_tan_F","srifle_DMR_03_multicam_F","srifle_DMR_03_woodland_F","srifle_DMR_06_camo_F","srifle_DMR_06_olive_F","srifle_DMR_06_camo_khs_F","srifle_EBR_F","arifle_MXM_F","arifle_MXM_Black_F","arifle_MXM_khk_F","arifle_SPAR_03_blk_F","arifle_SPAR_03_khk_F","arifle_SPAR_03_snd_F",
+			//Sniper
+			"srifle_LRR_F","srifle_LRR_camo_F","srifle_LRR_tna_F",
+			//SAW
+			"LMG_Mk200_F","LMG_Mk200_black_F","arifle_MX_SW_F","arifle_MX_SW_Black_F","arifle_MX_SW_khk_F","arifle_MX_SW_Black_Pointer_F","arifle_SPAR_02_blk_F","arifle_SPAR_02_khk_F","arifle_SPAR_02_snd_F",
+			//MG
+			"MMG_02_camo_F","MMG_02_black_F","MMG_02_sand_F",
+		};
 		Launchers[] = {
 			//*BOTH*//
 			"launch_RPG7_F", "launch_RPG32_F", "launch_RPG32_ghex_F", "launch_RPG32_green_F", "launch_RPG32_camo_F", "launch_B_Titan_F", "launch_I_Titan_F", "launch_O_Titan_F", "launch_Titan_F", "launch_B_Titan_short_F", "launch_I_Titan_short_F", "launch_O_Titan_short_F", "launch_Titan_short_F", "launch_B_Titan_tna_F", "launch_B_Titan_short_tna_F", "launch_O_Titan_ghex_F", "launch_O_Titan_short_ghex_F", "launch_I_Titan_eaf_F", "launch_B_Titan_olive_F",
 			//*BLUEFOR*//
-			"launch_MRAWS_olive_F","launch_MRAWS_olive_rail_F","launch_MRAWS_green_F","launch_MRAWS_green_rail_F","launch_MRAWS_sand_F","launch_MRAWS_sand_rail_F","launch_MRAWS_green_broken_F","launch_NLAW_F",
+			"launch_MRAWS_olive_F","launch_MRAWS_olive_rail_F","launch_MRAWS_green_F","launch_MRAWS_green_rail_F","launch_MRAWS_sand_F","launch_MRAWS_sand_rail_F","launch_NLAW_F",
 		};
 		Handguns[] = {
 			//*BOTH*//
@@ -27,7 +56,37 @@ class arsenalConfig
 	};
 	class east
 	{
-		Rifles[] = {};
+		Rifles[] = {
+		//*BOTH*//
+			//SMG
+			"SMG_03_TR_black","SMG_03_TR_camo","SMG_03_TR_khaki","SMG_03_TR_hex","SMG_03C_TR_black","SMG_03C_TR_camo","SMG_03C_TR_khaki","SMG_03C_TR_hex","SMG_03_black","SMG_03_camo","SMG_03_khaki","SMG_03_hex","SMG_03C_black","SMG_03C_camo","SMG_03C_khaki","SMG_03C_hex","hgun_PDW2000_F",
+			//Rifles
+			"arifle_AK12_F","arifle_AK12_GL_F","arifle_AK12_lush_F","arifle_AK12_arid_F","arifle_AK12_GL_lush_F","arifle_AK12_GL_arid_F","arifle_MSBS65_F","arifle_MSBS65_GL_F","arifle_MSBS65_UBS_F","arifle_MSBS65_black_F","arifle_MSBS65_GL_black_F","arifle_MSBS65_UBS_black_F","arifle_MSBS65_sand_F","arifle_MSBS65_GL_sand_F","arifle_MSBS65_UBS_sand_F","arifle_MSBS65_camo_F","arifle_MSBS65_GL_camo_F","arifle_MSBS65_UBS_camo_F","arifle_SDAR_F",
+			//Carbines
+			"arifle_AKS_F",
+			//DMR
+			"srifle_DMR_06_hunter_F","arifle_MSBS65_Mark_F","arifle_MSBS65_Mark_black_F","arifle_MSBS65_Mark_sand_F","arifle_MSBS65_Mark_camo_F",
+			//SAW
+			"LMG_03_F",
+			//Shotgun
+			"sgun_HunterShotgun_01_F","sgun_HunterShotgun_01_sawedoff_F",
+		//*REDFOR*//
+			//SMG
+			"SMG_05_F","SMG_02_F",
+			//Rifles
+			"arifle_ARX_blk_F","arifle_ARX_ghex_F","arifle_ARX_hex_F",
+			//Carbines
+			"arifle_AK12U_F","arifle_AK12U_lush_F","arifle_AK12U_arid_F","arifle_Katiba_C_F",
+			//DMR
+			"srifle_DMR_04_F","srifle_DMR_04_Tan_F","srifle_DMR_07_blk_F","srifle_DMR_07_hex_F","srifle_DMR_07_ghex_F","srifle_DMR_05_blk_F","srifle_DMR_05_hex_F","srifle_DMR_05_tan_f","srifle_DMR_01_F",
+			//Sniper
+			"srifle_GM6_F","srifle_GM6_camo_F","srifle_GM6_ghex_F",
+			//SAW
+			"arifle_CTARS_blk_F","arifle_CTARS_hex_F","arifle_CTARS_ghex_F","arifle_RPK12_F","arifle_RPK12_lush_F","arifle_RPK12_arid_F","LMG_Zafir_F",
+			//MG
+			"MMG_01_hex_F","MMG_01_tan_F","MMG_01_hex_ARCO_LP_F",
+		};
+
 		Launchers[] = {
 			//*BOTH*//
 			"launch_RPG7_F", "launch_RPG32_F", "launch_RPG32_ghex_F", "launch_RPG32_green_F", "launch_RPG32_camo_F", "launch_B_Titan_F", "launch_I_Titan_F", "launch_O_Titan_F", "launch_Titan_F", "launch_B_Titan_short_F", "launch_I_Titan_short_F", "launch_O_Titan_short_F", "launch_Titan_short_F", "launch_B_Titan_tna_F", "launch_B_Titan_short_tna_F", "launch_O_Titan_ghex_F", "launch_O_Titan_short_ghex_F", "launch_I_Titan_eaf_F", "launch_B_Titan_olive_F",
