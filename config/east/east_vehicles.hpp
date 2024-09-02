e41c43abcfd6a48d685dca6bdb57292718168c62
WeasleyWalls <weasley.walls@gmail.com>
2024-09-02 18:18:32 +0200

    aps adjustments (#336)

--- config/east/east_vehicles.hpp
+++ config/east/east_vehicles.hpp
@@ -284,7 +284,7 @@ class Vehicles {
         rearm = 1800;
         killReward = 600;
         capValue = 4;
-        aps = 2;
+        aps = 1;
     }; // "2S9 Sochor"
 
     class I_Truck_02_MRL_F {
@@ -293,7 +293,7 @@ class Vehicles {
         rearm = 1800;
         killReward = 600;
         capValue = 4;
-        aps = 2;
+        aps = 1;
     }; // "Zamak MRL"
 
     class O_Truck_03_medical_F {
