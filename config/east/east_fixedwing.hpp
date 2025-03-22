7ae5cbab831d97853c69d6934e0a080d80e92440
rook <rukqoa@gmail.com>
2025-03-22 16:22:34 -0700

    Mission testing misc fixes and QOL improvements, better GPS munitions

--- config/east/east_fixedwing.hpp
+++ config/east/east_fixedwing.hpp
@@ -234,11 +234,14 @@ class FixedWing {
             {"Bomb_03_F", "ammo_kab250kr"}
         };
         allowPylonMagazines[] = {
-            "PylonMissile_Bomb_KAB250_x1"
+            "PylonMissile_Bomb_KAB250_x1",
+            "PylonMissile_Missile_KH58_INT_x1"
         };
         cost = 32000;
         description = "To-201 Shikra (Standoff) is a variant of the To-201 Shikra armed with KAB-250KR bombs, which can be linked to and controlled at ground support terminals after release. How to use remote bombs:<br/>1. Have a teammate (player 2) build a ground support terminal.<br/>2. Player 2: Click on map, link to asset.<br/>3. Player 2: Control asset from terminal.<br/>4. Player 1: Drop KABs.";
-        disallowMagazines[] = {};
+        disallowMagazines[] = {
+            "PylonMissile_Missile_KH58_x1"
+        };
         hasHMD = 1;
         killReward = 1200;
         name = "To-201 Shikra (Standoff)";
