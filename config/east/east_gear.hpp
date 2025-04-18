f66e3e71df04dfec07873aff349f686270bd0d68
rook <5867121+spooker0@users.noreply.github.com>
2025-04-18 01:49:56 -0700

    More sound settings and custom gear container.

--- config/east/east_gear.hpp
+++ config/east/east_gear.hpp
@@ -1,67 +1,30 @@
 class Gear {
-    class Box_East_Ammo_F {
-        cost = 50;
-        killReward = 40;
-        loadable[] = { 0, 0, 0 };
-        offset[] = { 0, 3, 0};
-        requirements[] = {};
-    }; // "Basic Ammo [CSAT]"
+    class O_supplyCrate_launchers_F {
+        container[] = {
+            { "launch_O_Vorona_brown_F", 2 },
+            { "launch_RPG32_F", 2 },
+            { "launch_O_Titan_F", 2 },
+            { "launch_O_Titan_short_F", 2 },
 
-    class Box_East_Grenades_F {
-        cost = 50;
-        killReward = 40;
-        loadable[] = { 0, 0, 0 };
-        offset[] = { 0, 3, 0};
-        requirements[] = {};
-    }; // "Grenades [CSAT]"
+            { "RPG32_F", 8 },
+            { "RPG32_HE_F", 2 },
+            { "Titan_AA", 8 },
+            { "Titan_AP", 2 },
+            { "Titan_AT", 8 },
+            { "Vorona_HE", 2 },
+            { "Vorona_HEAT", 8 },
 
-    class Box_East_Wps_F {
-        cost = 100;
-        killReward = 40;
-        loadable[] = { 0, 0, 0 };
-        offset[] = { 0, 3, 0};
-        requirements[] = {};
-    }; // "Basic Weapons [CSAT]"
-
-    class Box_CSAT_Equip_F {
-        cost = 100;
-        killReward = 40;
-        loadable[] = { 0, 0, 0 };
-        offset[] = { 0, 3, 0};
-        requirements[] = {};
-    }; // "Equipment [NATO]"
-
-    class Box_CSAT_Uniforms_F {
-        cost = 100;
-        killReward = 40;
-        loadable[] = { 0, 0, 0 };
-        offset[] = { 0, 3, 0};
-        requirements[] = {};
-    }; // "Uniforms [NATO]"
-
-    class Box_East_AmmoOrd_F {
-        cost = 150;
-        killReward = 40;
-        loadable[] = { 0, 0, 0 };
-        offset[] = { 0, 3, 0};
-        requirements[] = {};
-    }; // "Explosives [CSAT]"
-
-    class Box_East_WpsLaunch_F {
+            { "B_FieldPack_ocamo", 5 }
+        };
         cost = 200;
-        killReward = 40;
+        demolishable = 1;
+        killReward = 200;
         loadable[] = { 0, 0, 0 };
+        name = "CSAT Supply Crate";
         offset[] = { 0, 3, 0};
+        spawn = "CargoNet_01_box_F";
         requirements[] = {};
-    }; // "Launchers [CSAT]"
-
-    class Box_East_WpsSpecial_F {
-        cost = 250;
-        killReward = 40;
-        loadable[] = { 0, 0, 0 };
-        offset[] = { 0, 3, 0};
-        requirements[] = {};
-    }; // "Special Weapons [CSAT]"
+    };
 
     class Box_East_AmmoVeh_F {
         cost = 250;
@@ -70,14 +33,6 @@ class Gear {
         offset[] = { 0, 3, 0};
         requirements[] = {};
     }; // "Vehicle Ammo [CSAT]"
-
-    class O_supplyCrate_F {
-        cost = 300;
-        killReward = 40;
-        loadable[] = { 0, 0, 0 };
-        offset[] = { 0, 3, 0};
-        requirements[] = {};
-    }; // "Supply Box [CSAT]"
 
     class Land_Pod_Heli_Transport_04_fuel_F {
         cost = 300;
