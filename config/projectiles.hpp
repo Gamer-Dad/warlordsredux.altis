88cfa865e1b70f7695e83fb97117492c8b487b3e
rook <5867121+spooker0@users.noreply.github.com>
2025-04-21 12:02:27 -0700

    Fixes for test session.

--- config/projectiles.hpp
+++ config/projectiles.hpp
@@ -64,7 +64,7 @@ class WLProjectilesConfig {
         aps = 3;
         camera = 1;
         sam = 1;
-        speed = 1.5;
+        speed = 1.25;
     };
 
 
@@ -75,7 +75,7 @@ class WLProjectilesConfig {
         aps = 3;
         camera = 1;
         sam = 1;
-        speed = 2;
+        speed = 1.5;
     };
 
     class WLProjectile_A2A: WLProjectile {
