b44568df5c40d09a9ca2eceed6f59813cb1aa7fe
rook <5867121+spooker0@users.noreply.github.com>
2025-06-13 17:34:02 -0700

    Hercules to replace RIMs.

--- config/west/west_airdefense.hpp
+++ config/west/west_airdefense.hpp
@@ -211,30 +211,47 @@ class AirDefense {
         };
     };
 
-    class B_SAM_System_LRAA_F {
+    class B_SAM_System_01_Advanced_F {
         ammoOverrides[] = {
-            {"ammo_Missile_Cruise_01", {"M_SM6", "Standard Missile 6 Block II"}}
+            {"ammo_Missile_rim116", {"M_RIM116B", "Hercules AA"}}
         };
-        cost = 42000;
-        description = "Extended range air-defense system, capable of aggressively engaging aircraft and helicopters. Effective range: >50km.";
-        disallowMagazines[] = {
-            "magazine_Missiles_Cruise_01_Cluster_x18"
-        };
-        hasESAM = 1;
+        cost = 25000;
+        description = "Advanced surface-to-air missile system, capable of aggressively engaging aircraft and helicopters. Effective range: 14km. Use SAM configuration menu to select data-linked targets outside of visual range.";
+        hasASAM = 1;
         hasHMD = 1;
-        name = "RIM-174 Standard ERAM";
-        rearm = 2700;
+        loadable[] = {0, -2, 0.8};
+        name = "Mk59 Hercules";
+        offset[] = {0, 5.3, 0};
+        rearm = 360;
         requirements[] = {};
-        spawn = "B_Ship_MRLS_01_F";
-
-        class Gunner: WLTurretDefaults {
-            addMagazines[] = {};
-            addWeapons[] = {};
-            turret[] = { 0 };
-            removeMagazines[] = {
-                "magazine_Missiles_Cruise_01_Cluster_x18"
-            };
-            removeWeapons[] = {};
-        };
+        spawn = "B_SAM_System_01_F";
+        variant = 1;
     };
+
+    // class B_SAM_System_LRAA_F {
+    //     ammoOverrides[] = {
+    //         {"ammo_Missile_Cruise_01", {"M_SM6", "Standard Missile 6 Block II"}}
+    //     };
+    //     cost = 42000;
+    //     description = "Extended range air-defense system, capable of aggressively engaging aircraft and helicopters. Effective range: >50km.";
+    //     disallowMagazines[] = {
+    //         "magazine_Missiles_Cruise_01_Cluster_x18"
+    //     };
+    //     hasESAM = 1;
+    //     hasHMD = 1;
+    //     name = "RIM-174 Standard ERAM";
+    //     rearm = 2700;
+    //     requirements[] = {};
+    //     spawn = "B_Ship_MRLS_01_F";
+
+    //     class Gunner: WLTurretDefaults {
+    //         addMagazines[] = {};
+    //         addWeapons[] = {};
+    //         turret[] = { 0 };
+    //         removeMagazines[] = {
+    //             "magazine_Missiles_Cruise_01_Cluster_x18"
+    //         };
+    //         removeWeapons[] = {};
+    //     };
+    // };
 };
