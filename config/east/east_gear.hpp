10cbfe75bdb1fd651c24edb0a31fdcf23db2d172
spooker0 <5867121+spooker0@users.noreply.github.com>
2025-06-29 06:15:36 -0700

    Added bipods  (#892)
    * SEAD changes, sector reset fixes, scanner changes, misc fixes, rearm rebalance.
    
    * Added bipods

--- config/east/east_gear.hpp
+++ config/east/east_gear.hpp
@@ -23,6 +23,8 @@ class Gear {
             { "SatchelCharge_Remote_Mag", 3},
             { "SLAMDirectionalMine_Wire_Mag", 3},
 
+            { "bipod_02_F_tan", 5 },
+
             { "B_FieldPack_ocamo", 5 },
             { "B_Carryall_ocamo", 5 }
         };
