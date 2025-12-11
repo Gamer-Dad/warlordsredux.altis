bd5d8778669a27e05ef5cbab67fa77320d16e33a
spooker0 <5867121+spooker0@users.noreply.github.com>
2025-12-11 15:48:21 -0800

    Reduced growler air nerf.

--- config/west/west_fixedwing.hpp
+++ config/west/west_fixedwing.hpp
@@ -323,7 +323,7 @@ class FixedWing {
             "PylonRack_Missile_AGM_02_x2"
         };
         ecm[] = {{"MissileCore"}, 0, 30000, 1, 2, 120};
-        hasAirRadar = 8000;
+        hasAirRadar = 15000;
         hasHMD = 1;
         name = "EF/A-181 Growler";
         rearm = 420;
